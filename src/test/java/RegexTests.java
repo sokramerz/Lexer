10d757be1ed5a46988da80a1b71012b21664d8c0
sokramerz <10010541@sfcollege.edu>
2026-01-31 20:19:42 -0500

    First commit

---
+++ src/test/java/RegexTests.java
@@ -0,0 +1,127 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * Contains JUnit tests for {@link Regex}. A framework of the test structure 
+ * is provided, you will fill in the remaining pieces.
+ *
+ * To run tests, either click the run icon on the left margin, which can be used
+ * to run all tests or only a specific test. You should make sure your tests are
+ * run through IntelliJ (File > Settings > Build, Execution, Deployment > Build
+ * Tools > Gradle > Run tests using <em>IntelliJ IDEA</em>). This ensures the
+ * name and inputs for the tests are displayed correctly in the run window.
+ */
+public class RegexTests {
+
+    /**
+     * This is a parameterized test for the {@link Regex#EMAIL} regex. The
+     * {@link ParameterizedTest} annotation defines this method as a
+     * parameterized test, and {@link MethodSource} tells JUnit to look for the
+     * static method {@link #testEmailRegex()}.
+     *
+     * For personal preference, I include a test name as the first parameter
+     * which describes what that test should be testing - this is visible in
+     * IntelliJ when running the tests (see above note if not working).
+     */
+    @ParameterizedTest
+    @MethodSource
+    public void testEmailRegex(String test, String input, boolean success) {
+        test(input, Regex.EMAIL, success);
+    }
+
+    /**
+     * This is the factory method providing test cases for the parameterized
+     * test above - note that it is static, takes no arguments, and has the same
+     * name as the test. The {@link Arguments} object contains the arguments for
+     * each test to be passed to the function above.
+     */
+    public static Stream<Arguments> testEmailRegex() {
+        return Stream.of(
+                Arguments.of("Alphanumeric", "thelegend27@gmail.com", true),
+                Arguments.of("UF Domain", "otherdomain@ufl.edu", true),
+                Arguments.of("Missing Domain Dot", "missingdot@gmailcom", false),
+                Arguments.of("Symbols", "symbols#$%@gmail.com", false),
+
+                // Matching cases new tests
+                Arguments.of("Top-Level Domain Length", "thepresident@gmail.xi", true),
+                Arguments.of("Numeric", "747747@fly.com", true),
+                Arguments.of("Special Char", "Hello-World.Its.Me@hotmail.com", true),
+
+                // Remaining 3 false tests
+                Arguments.of("Top-Level Domain Length", "otherdomain@ufl.info", false),
+                Arguments.of("Domain", "Idonotlikejava@UFL_csc.edu", false),
+                Arguments.of("Top-Level Domain Cap", "itssofree@gmail.COM", false),
+
+                // Extra Tests to cover all
+                Arguments.of("No @", "feelgoodgmail.com", false),
+                Arguments.of("Special Char", "heyitsme!#@gmail.com", false),
+                Arguments.of("Domain .", "dontforgetperiod@gmailcom", false),
+                Arguments.of("Domain Spec Char", "Ilovejava@ufl-edu.csc", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testOddStringsRegex(String test, String input, boolean success) {
+        test(input, Regex.ODD_STRINGS, success);
+    }
+
+    public static Stream<Arguments> testOddStringsRegex() {
+        return Stream.of(
+                // what have eleven letters and starts with gas?
+                Arguments.of("11 Characters", "automobiles", true),
+                Arguments.of("13 Characters", "i<3pancakes13", true),
+                Arguments.of("5 Characters", "5five", false),
+                Arguments.of("14 Characters", "i<3pancakes14!", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testIntegerListRegex(String test, String input, boolean success) {
+        test(input, Regex.INTEGER_LIST, success);
+    }
+
+    public static Stream<Arguments> testIntegerListRegex() {
+        return Stream.of(
+                Arguments.of("Single Element", "[1]", true),
+                Arguments.of("Multiple Elements", "[1,20,3]", true),
+                Arguments.of("Missing Brackets", "1,2,3", false),
+                Arguments.of("Missing Commas", "[1 2 3]", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testDecimalRegex(String test, String input, boolean success) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    public static Stream<Arguments> testDecimalRegex() {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testStringRegex(String test, String input, boolean success) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public static Stream<Arguments> testStringRegex() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    /**
+     * Asserts that the input matches the given pattern. This method doesn't do
+     * much now, but you will see this concept in future assignments.
+     */
+    private static void test(String input, Pattern pattern, boolean success) {
+        Assertions.assertEquals(success, pattern.matcher(input).matches());
+    }
+
+}
